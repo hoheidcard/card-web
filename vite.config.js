1c90ac492b93b9be4e05c8c0c60c260331e76498
ankitjain0748 <ankitjain0748@gmail.com>
2025-05-16 13:06:28 +0530

    manage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+server: {
+  host: '0.0.0.0',
+  port: 5173,
+  strictPort: true,
+  hmr: {
+    host: 'abhitprints.in'
+  },
+  // 🚨 Not safe for production!
+  allowedHosts: 'all'
+}
